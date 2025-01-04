fb00d8ae79347360fb8ca413da87e701befcd76c
Katya <katya.p.1129@gmail.com>
2025-01-04 16:23:35 -0600

    add server actions, update sidebar w docs

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import { useCollection } from "react-firebase-hooks/firestore";
 import NewDocumentButton from "./NewDocumentButton";
 import {
   Sheet,
@@ -11,11 +13,84 @@ import {
 
 import { Button } from "@/components/ui/button";
 import { MenuIcon } from "lucide-react";
+import { useUser } from "@clerk/nextjs";
+import {
+  collectionGroup,
+  DocumentData,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "@/firebase";
+import { useEffect, useState } from "react";
+
+interface RoomDocument extends DocumentData {
+  createdAt: string;
+  role: "owner" | "editor";
+  roomId: string;
+  userId: string;
+}
 
 function Sidebar() {
+  const { user } = useUser();
+  const [groupedData, setGroupedData] = useState<{
+    owner: RoomDocument[];
+    editor: RoomDocument[];
+  }>({
+    owner: [],
+    editor: [],
+  });
+  const [data, loading, error] = useCollection(
+    user &&
+      query(
+        collectionGroup(db, "rooms"),
+        where("userId", "==", user.emailAddresses[0].toString())
+      )
+  );
+
+  useEffect(() => {
+    if (!data) return;
+
+    const grouped = data.docs.reduce<{
+      owner: RoomDocument[];
+      editor: RoomDocument[];
+    }>(
+      (acc, curr) => {
+        const roomData = curr.data() as RoomDocument;
+
+        if (roomData.role === "owner") {
+          acc.owner.push({
+            id: curr.id,
+            ...roomData,
+          });
+        } else {
+          acc.editor.push({
+            id: curr.id,
+            ...roomData,
+          });
+        }
+
+        return acc;
+      },
+      {
+        owner: [],
+        editor: [],
+      }
+    );
+    setGroupedData(grouped);
+  }, [data]);
+
   const menuOptions = (
     <>
       <NewDocumentButton />
+
+      {/* My Docs */}
+      {groupedData.owner.length === 0 ? (
+        <h2>No documents found</h2>
+      ) : (
+        <p>{groupedData.owner.length} documents found</p>
+      )}
+
+      {/* Shared Docs */}
     </>
   );
   return (
