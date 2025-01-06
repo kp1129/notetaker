a5107b5e034ac6406cdaa8b8b36a6b5fe5c3c4ad
Katya <katya.p.1129@gmail.com>
2025-01-05 21:05:05 -0600

    add manage users

--- components/Document.tsx
+++ components/Document.tsx
@@ -10,6 +10,7 @@ import Editor from "./Editor";
 import useOwner from "@/lib/useOwner";
 import DeleteDocument from "./DeleteDocument";
 import InviteUser from "./InviteUser";
+import ManageUsers from "./ManageUsers";
 
 function Document({ id }: { id: string }) {
   const [data, loading, error] = useDocumentData(doc(db, "documents", id));
@@ -52,6 +53,10 @@ function Document({ id }: { id: string }) {
             </>
           )}
         </form>
+      </div>
+
+      <div className="flex max-w-6xl mx-auto justify-between items-center mb-5">
+        <ManageUsers />
       </div>
 
       <hr className="pb-10" />
