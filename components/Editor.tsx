2c8afdfc0203210919aa33437b2a190abf358fd9
Katya <katya.p.1129@gmail.com>
2025-01-05 23:41:54 -0600

    add AI summary/translation

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -13,6 +13,7 @@ import { useCreateBlockNote } from "@blocknote/react";
 import "@blocknote/core/fonts/inter.css";
 import "@blocknote/shadcn/style.css";
 import stringToColor from "@/lib/stringToColor";
+import TranslateDocument from "./TranslateDocument";
 
 type EditorProps = {
   doc: Y.Doc;
@@ -77,8 +78,10 @@ function Editor() {
     <div className="max-w-6xl mx-auto">
       <div className="flex items-center gap-2 justify-end mb-10">
         {/* Translate - AI FEATURE */}
+        <TranslateDocument doc={doc} />
 
         {/* Chat - AI FEATURE */}
+        {/* <ChatToDocument doc={doc} /> */}
 
         <Button className={style} onClick={() => setDarkMode(!darkMode)}>
           {darkMode ? <SunIcon /> : <MoonIcon />}
